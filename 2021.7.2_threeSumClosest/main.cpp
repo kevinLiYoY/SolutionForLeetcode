31fe4abfe7d7f7933b7b4919306c75571e163ce5
KevinLi <mifetan@foxmail.com>
2021-07-02 23:58:18 +0800

    push commit

--- 2021.7.2_threeSumClosest/main.cpp
+++ 2021.7.2_threeSumClosest/main.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-//方法1, 遍历并求出当前和，并依据三数和为零求出空缺位置数值，在余下的数字中寻找目标值，如果找到，则放入输出结果列表中
+//方法1, 遍历并求出当前和，
 
 bool findMatchNum(int iMatchSum, int index, vector<int>& nums)
 {
@@ -55,6 +55,68 @@ int threeSumClosest(vector<int>& nums, int target)
     return result;
 }
 
+//官方解题，使用双指针减少时间复杂度
+int threeSumClosest2(vector<int>& nums, int target)
+{
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    int best = 1e7;
+
+    // 根据差值的绝对值来更新答案
+    auto update = [&](int cur) 
+    {
+        if (abs(cur - target) < abs(best - target))
+        {
+            best = cur;
+        }
+    };
+
+    // 枚举 a
+    for (int i = 0; i < n; ++i)
+    {
+        // 保证和上一次枚举的元素不相等
+        if (i > 0 && nums[i] == nums[i - 1])
+        {
+            continue;
+        }
+        // 使用双指针枚举 b 和 c
+        int j = i + 1, k = n - 1;
+        while (j < k)
+        {
+            int sum = nums[i] + nums[j] + nums[k];
+            // 如果和为 target 直接返回答案
+            if (sum == target)
+            {
+                return target;
+            }
+            update(sum);
+            if (sum > target)
+            {
+                // 如果和大于 target，移动 c 对应的指针
+                int k0 = k - 1;
+                // 移动到下一个不相等的元素
+                while (j < k0 && nums[k0] == nums[k])
+                {
+                    --k0;
+                }
+                k = k0;
+            }
+            else
+            {
+                // 如果和小于 target，移动 b 对应的指针
+                int j0 = j + 1;
+                // 移动到下一个不相等的元素
+                while (j0 < k && nums[j0] == nums[j])
+                {
+                    ++j0;
+                }
+                j = j0;
+            }
+        }
+    }
+    return best;
+}
+
 
 int main()
 {
@@ -86,6 +148,8 @@ int main()
 
         int result = threeSumClosest(muns, target);
         std::cout << "****************************************************Result: " << result << std::endl;
+        result = threeSumClosest2(muns, target);
+        std::cout << "****************************************************Result2: " << result << std::endl;
     }
 
     return 0;
